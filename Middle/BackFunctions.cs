b7b35e31b04437becd70b025489470ca960f200f
Jacob Guin <technogeekntraining@gmail.com>
2024-10-16 14:20:56 -0400

    Removed Static Class
    The Function now requires the class to be constructed.
    The Function class now has a base path that can be changed per instance.

--- Middle/BackFunctions.cs
+++ Middle/BackFunctions.cs
@@ -2,16 +2,15 @@ using System.Diagnostics;
 
 namespace Middle;
 
-public static class BackFunctions
+public class BackFunctions
 {
     /// <summary>
     /// Testing Dictionary for function will be moved config files after the the user can update it in the UI
     /// </summary>
-    public static Dictionary<string, string> ProgramMap = new();//Testing for now
+    public Dictionary<string, string> ProgramMap = new();//Testing for now
 
-    public static void LoadMaps()
+    public BackFunctions()
     {
-        ProgramMap.Clear();
         if (OperatingSystem.IsWindows())
         {
             ProgramMap.Add(".txt", "notepad.exe");
@@ -22,8 +21,18 @@ public static class BackFunctions
         }
     }
 
-    public static void Open(string FileToOpen)
+    public string? GetProgram(string FileName)
     {
+        FileInfo FI = new FileInfo(FileName);
+        if (ProgramMap.TryGetValue(FI.Extension.ToLower(), out string? Program)) return Program;
+        return null;
+    }
+    
+    public required string BasePath { get; set; }
+
+    public void Open(string FileToOpen)
+    {
+        FileToOpen = Path.Combine(BasePath, FileToOpen);
         FileInfo FI = new FileInfo(FileToOpen);
         
         if (ProgramMap.TryGetValue(FI.Extension.ToLower(), out string Program))
@@ -34,8 +43,9 @@ public static class BackFunctions
         Process.Start(FileToOpen);
     }
     
-    public static void OpenWith(string Program, string FileToOpen)
+    public void OpenWith(string Program, string FileToOpen)
     {
+        FileToOpen = Path.Combine(BasePath, FileToOpen);
         Process p = new Process()
         {
             StartInfo =
@@ -47,8 +57,9 @@ public static class BackFunctions
         _ = p.Start();
     }
 
-    public static void CreateFile(string FileName)
+    public void CreateFile(string FileName)
     {
+        FileName = Path.Combine(BasePath, FileName);
         if (File.Exists(FileName))
         {
             Console.WriteLine($"File '{FileName}' already exists");
@@ -65,8 +76,9 @@ public static class BackFunctions
 
     }
 
-    public static void DeleteFile(string FileName)
+    public void DeleteFile(string FileName)
     {
+        FileName = Path.Combine(BasePath, FileName);
         if (File.Exists(FileName))
         {
             File.Delete(FileName);
@@ -78,8 +90,9 @@ public static class BackFunctions
         }
     }
 
-    public static void CreateFolder(string FolderName)
+    public void CreateFolder(string FolderName)
     {
+        FolderName = Path.Combine(BasePath, FolderName);
         if (!Directory.Exists(FolderName))
         {
             Directory.CreateDirectory(FolderName);
