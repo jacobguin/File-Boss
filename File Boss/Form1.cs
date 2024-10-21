7d8ab9fa4eb58fcfd0da94bcfeac0e0ec1f0647d
camsdean <camer@LAPTOP-E5F20IQ2>
2024-10-21 16:04:00 -0400

    Revert "merge"
    This reverts commit 69d416301c61bfeae4d1660d4fa840edeb56e6b3.

--- File Boss/Form1.cs
+++ File Boss/Form1.cs
@@ -1,5 +1,9 @@
+using Middle;
 using System.Diagnostics;
+using System.Drawing;
 using System.Reflection;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace File_Boss
 {
@@ -9,42 +13,84 @@ namespace File_Boss
         {
             //DO NOT TOUCH
             InitializeComponent();
+            functionHandler = new() {BasePath = Directory.GetCurrentDirectory() };
             DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
             foreach (FileInfo fi in di.GetFiles())
             {
-                FileDisplay fd = new();
-                Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName)!;
-                fd.LoadFile(fi.FullName, icon);
-                flowLayoutPanel1.Controls.Add(fd);
+                addFileDisplay(fi);
             }
         }
 
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
+        ///Allows for the creation of files
+        public BackFunctions functionHandler;
 
+        /// <summary>
+        /// New on click function to select files in the flowLayoutPanel1
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private Task Fd_OnAllClick(FileDisplay arg)
+        {
+            MessageBox.Show(arg.label1.Text);
+            return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Add Button which creates a text field for you to input a file name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             TextBox textBox = new TextBox();
-            //textBox.Name = "FileName";
-            textBox.Location = new System.Drawing.Point(648, 375);
+            textBox.Location = new System.Drawing.Point(86, 16);
             textBox.Size = new System.Drawing.Size(125, 27);
             this.Controls.Add(textBox);
-            
-
-            Middle.BackFunctions.CreateFile("newfile2.txt");
-
-
-
-
+            textBox.KeyPress += TextBox_KeyPress;
         }
 
-        
-
-        private void button2_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Once ENTER is pressed, a file is created and a message box appears
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_KeyPress(object? sender, KeyPressEventArgs e)
         {
-            Middle.BackFunctions.DeleteFile();
+            if (e.KeyChar != (char)Keys.Enter) return;
+
+            TextBox temp = (TextBox)sender!;
+            functionHandler.CreateFile(temp.Text);
+            MessageBox.Show(temp.Text + " was Created!");
+            addFileDisplay(new FileInfo(temp.Text));
+            Controls.Remove(temp);
+        }
+
+        /// <summary>
+        /// Event Handlers, which change the color of the Add Button if it is hovered over or not
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.RoyalBlue;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.CornflowerBlue;
+        }
+
+        /// <summary>
+        /// Backend function in order to display file information
+        /// </summary>
+        /// <param name="fi"></param>
+        public void addFileDisplay(FileInfo fi)
+        {
+            FileDisplay fd = new();
+            Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName)!;
+            fd.LoadFile(fi.FullName, icon, functionHandler);
+            fd.OnAllClick += Fd_OnAllClick;
+            flowLayoutPanel1.Controls.Add(fd);
         }
     }
 }
