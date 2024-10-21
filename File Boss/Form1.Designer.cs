69d416301c61bfeae4d1660d4fa840edeb56e6b3
Davis Hagle <DBHAGL2644@ung.edu>
2024-10-21 16:01:07 -0400

    merge

--- File Boss/Form1.Designer.cs
+++ File Boss/Form1.Designer.cs
@@ -30,40 +30,50 @@
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
             button1 = new Button();
+            button2 = new Button();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            flowLayoutPanel1.Dock = DockStyle.Bottom;
-            flowLayoutPanel1.Location = new Point(0, 61);
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(800, 390);
+            flowLayoutPanel1.Size = new Size(582, 450);
             flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
             // 
             // button1
             // 
-            button1.BackColor = Color.CornflowerBlue;
-            button1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(0, 0);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(3, 3);
             button1.Name = "button1";
-            button1.Size = new Size(82, 64);
-            button1.TabIndex = 1;
-            button1.Text = "+";
-            button1.UseVisualStyleBackColor = false;
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
-            button1.MouseEnter += button1_MouseEnter;
-            button1.MouseLeave += button1_MouseLeave;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(103, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Delete\r\n";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
-            Controls.Add(button1);
+            ClientSize = new Size(800, 450);
             Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -71,5 +81,6 @@
 
         private FlowLayoutPanel flowLayoutPanel1;
         private Button button1;
+        private Button button2;
     }
 }
--- File Boss/Form1.cs
+++ File Boss/Form1.cs
@@ -1,9 +1,5 @@
-using Middle;
 using System.Diagnostics;
-using System.Drawing;
 using System.Reflection;
-using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 namespace File_Boss
 {
@@ -13,84 +9,42 @@ namespace File_Boss
         {
             //DO NOT TOUCH
             InitializeComponent();
-            functionHandler = new() {BasePath = Directory.GetCurrentDirectory() };
             DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
             foreach (FileInfo fi in di.GetFiles())
             {
-                addFileDisplay(fi);
+                FileDisplay fd = new();
+                Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName)!;
+                fd.LoadFile(fi.FullName, icon);
+                flowLayoutPanel1.Controls.Add(fd);
             }
         }
 
-        ///Allows for the creation of files
-        public BackFunctions functionHandler;
-
-        /// <summary>
-        /// New on click function to select files in the flowLayoutPanel1
-        /// </summary>
-        /// <param name="arg"></param>
-        /// <returns></returns>
-        private Task Fd_OnAllClick(FileDisplay arg)
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-            MessageBox.Show(arg.label1.Text);
-            return Task.CompletedTask;
+
         }
 
-        /// <summary>
-        /// Add Button which creates a text field for you to input a file name
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             TextBox textBox = new TextBox();
-            textBox.Location = new System.Drawing.Point(86, 16);
+            //textBox.Name = "FileName";
+            textBox.Location = new System.Drawing.Point(648, 375);
             textBox.Size = new System.Drawing.Size(125, 27);
             this.Controls.Add(textBox);
-            textBox.KeyPress += TextBox_KeyPress;
+            
+
+            Middle.BackFunctions.CreateFile("newfile2.txt");
+
+
+
+
         }
 
-        /// <summary>
-        /// Once ENTER is pressed, a file is created and a message box appears
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void TextBox_KeyPress(object? sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar != (char)Keys.Enter) return;
+        
 
-            TextBox temp = (TextBox)sender!;
-            functionHandler.CreateFile(temp.Text);
-            MessageBox.Show(temp.Text + " was Created!");
-            addFileDisplay(new FileInfo(temp.Text));
-            Controls.Remove(temp);
-        }
-
-        /// <summary>
-        /// Event Handlers, which change the color of the Add Button if it is hovered over or not
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_MouseEnter(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            button1.BackColor = Color.RoyalBlue;
-        }
-
-        private void button1_MouseLeave(object sender, EventArgs e)
-        {
-            button1.BackColor = Color.CornflowerBlue;
-        }
-
-        /// <summary>
-        /// Backend function in order to display file information
-        /// </summary>
-        /// <param name="fi"></param>
-        public void addFileDisplay(FileInfo fi)
-        {
-            FileDisplay fd = new();
-            Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName)!;
-            fd.LoadFile(fi.FullName, icon, functionHandler);
-            fd.OnAllClick += Fd_OnAllClick;
-            flowLayoutPanel1.Controls.Add(fd);
+            Middle.BackFunctions.DeleteFile();
         }
     }
 }
