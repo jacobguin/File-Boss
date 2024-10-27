f43eca24faf32e30e59bce41a8a8f5e0f7e14994
vKilljoy <claypoore728@gmail.com>
2024-10-27 19:53:10 -0400

    UI Changes to zip a folder
    Added a button that can be accessed when right clicking a folder that will zip the folder

--- File Boss/ItemView.cs
+++ File Boss/ItemView.cs
@@ -12,68 +12,89 @@ using System.Windows.Forms;
 
 namespace File_Boss
 {
-	public partial class ItemView : UserControl
-	{
-		public ItemView()
-		{
-			InitializeComponent();
-		}
+    public partial class ItemView : UserControl
+    {
+        public ItemView()
+        {
+            InitializeComponent();
+        }
 
-		private BackFunctions functionHandler;
-		public FileInfo? CurentFile;
-		public DirectoryInfo? CurentDirectory;
-		public event Func<ItemView, Task>? OnAllClick;
-		public event Func<ItemView, Task>? OnDelete;
+        private BackFunctions functionHandler;
+        public FileInfo? CurentFile;
+        public DirectoryInfo? CurentDirectory;
+        public event Func<ItemView, Task>? OnAllClick;
+        public event Func<ItemView, Task>? OnDelete;
 
-		private void LoadBoth(BackFunctions functionHandler)
-		{
-			this.functionHandler = functionHandler;
-			this.MouseDoubleClick += label1_Click;
-			label1.MouseDoubleClick += label1_Click;
-			pictureBox1.MouseDoubleClick += label1_Click;
-		}
+        private void LoadBoth(BackFunctions functionHandler)
+        {
+            this.functionHandler = functionHandler;
+            this.MouseDoubleClick += label1_Click;
+            label1.MouseDoubleClick += label1_Click;
+            pictureBox1.MouseDoubleClick += label1_Click;
 
-		public void LoadFolder(string Folder, BackFunctions functionHandler)
-		{
-			LoadBoth(functionHandler);
-			CurentDirectory = new(Folder);
-			label1.Text = CurentDirectory.Name;
-			contextMenuStrip1.Items.Remove(openWithToolStripMenuItem);
-		}
+            var zipMenuItem = new ToolStripMenuItem("Zip Folder");
+            zipMenuItem.Click += ZipMenuItem_Click;
+            contextMenuStrip1.Items.Add(zipMenuItem);
+        }
 
-		public void LoadFile(string File, Icon icon, BackFunctions functionHandler)
-		{
-			LoadBoth(functionHandler);
-			CurentFile = new(File);
-			label1.Text = CurentFile.Name;
-			pictureBox1.Image = icon.ToBitmap();
-			openWithToolStripMenuItem.DropDownItems.Clear();
-			foreach (var program in functionHandler.ProgramMap.Values)
-			{
-				ToolStripMenuItem programItem = new(program);
-				openWithToolStripMenuItem.DropDownItems.Add(programItem);
+        public void LoadFolder(string Folder, BackFunctions functionHandler)
+        {
+            LoadBoth(functionHandler);
+            CurentDirectory = new(Folder);
+            label1.Text = CurentDirectory.Name;
+            contextMenuStrip1.Items.Remove(openWithToolStripMenuItem);
+        }
 
-				programItem.Click += (s, args) =>
-				{
-					functionHandler.OpenWith(program, CurentFile.FullName);
-				};
-			}
-		}
+        public void LoadFile(string File, Icon icon, BackFunctions functionHandler)
+        {
+            LoadBoth(functionHandler);
+            CurentFile = new(File);
+            label1.Text = CurentFile.Name;
+            pictureBox1.Image = icon.ToBitmap();
+            openWithToolStripMenuItem.DropDownItems.Clear();
+            foreach (var program in functionHandler.ProgramMap.Values)
+            {
+                ToolStripMenuItem programItem = new(program);
+                openWithToolStripMenuItem.DropDownItems.Add(programItem);
 
-		private void label1_Click(object sender, EventArgs e)
-		{
-			if (OnAllClick is not null)
-			{
-				OnAllClick.Invoke(this);
-			}
-		}
+                programItem.Click += (s, args) =>
+                {
+                    functionHandler.OpenWith(program, CurentFile.FullName);
+                };
+            }
+        }
+        private void ZipMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CurentDirectory != null && !CurentDirectory.Name.EndsWith(".zip"))
+            {
+                functionHandler.ZipFolder(CurentDirectory.FullName);
+                MessageBox.Show("Folder successfully zipped!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("This item is already zipped or is not a folder.", "Cannot Zip", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
 
-		private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			if (OnDelete is not null)
-			{
-				OnDelete.Invoke(this);
-			}
-		}
-	}
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (OnAllClick is not null)
+            {
+                OnAllClick.Invoke(this);
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OnDelete is not null)
+            {
+                OnDelete.Invoke(this);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
