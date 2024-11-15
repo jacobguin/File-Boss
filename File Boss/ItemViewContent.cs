6b3e1392281bf7f5d72bd26c48cf3451f46ef062
jacob guin <technogeekntraining@gmail.com>
2024-11-15 10:40:04 -0500

    Fixed anothoter issue from merge

--- File Boss/ItemViewContent.cs
+++ File Boss/ItemViewContent.cs
@@ -97,7 +97,10 @@ namespace File_Boss
 			pictureBox1.Image = icon.ToBitmap();
 			try
 			{
-				pictureBox1.Image = new Bitmap(CurrentFile.FullName);
+				using (Bitmap tempImage = new(CurrentFile.FullName))
+				{
+					pictureBox1.Image = new Bitmap(tempImage);
+				}
 			}
 			catch { }
 			openWithToolStripMenuItem.DropDownItems.Clear();
